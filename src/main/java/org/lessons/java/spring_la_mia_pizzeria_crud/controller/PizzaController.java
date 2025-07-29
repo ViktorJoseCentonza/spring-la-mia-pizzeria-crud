c64e2b411f35b86be8364416f5176f2cc89888e4
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-07-29 15:10:03 +0200

    add single page and links to the single pages in the list (+ show in the controller)

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -7,7 +7,9 @@ import org.lessons.java.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.ui.Model;
 
 @Controller
@@ -22,6 +24,13 @@ public class PizzaController {
         List<Pizza> pizzas = repository.findAll();
         model.addAttribute("pizzas", pizzas);
         return "pizzas";
+    }
+
+    @GetMapping("pizza/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+
+        model.addAttribute("pizza", repository.findById(id).get());
+        return "pizza";
     }
 
 }
